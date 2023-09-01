1b0c0aefe9fbf9d0a8b6494c04b18b6f45f61f03
Linda Lillian <lindalilliane32@gmail.com>
2023-09-02 00:14:06 +0300

    Ancestry specific mdd_extract data and script

--- scripts/extract_mdd.r
+++ scripts/extract_mdd.r
@@ -1,34 +1,42 @@
 library(data.table)
 library(dplyr)
-library(gwasvcf)
+#library(gwasvcf)
 library(here)
 library(tidyr)
 
-set_bcftools()
 
-prot <- readRDS(here("data", "combined_pqtl.rds"))
+# Reading and subsetting for only the 11 proteins 
+prot1 <- readRDS(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/data", "combined_pqtl.rds"))
+MDDprot <- read.table(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/data", "Allprotlist_chrposcode.txt"))
+names(MDDprot) <- c("chr", "pos", "rsid", "prot")
+prot <- subset(prot1, prot %in% MDDprot$prot)
 chrpos <- paste0(prot$chr, ":", prot$pos)
 
 # AFR
-afr <- query_gwas(here("scratch", "ukb-e-20126_p1_AFR.vcf.gz"), chrompos=chrpos)
-afr <- afr %>% vcf_to_tibble() %>%
-    mutate(pop="AFR")
+afr <- fread(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/scratch", "FE_afr_exclu_whi_jhs_23andMe_qced_rsid.txt.gz"))
+afr <- subset(afr, rsid %in% prot$rsid)
+dim(afr)
 
-# CSA
-csa <- query_gwas(here("scratch", "ukb-e-20126_p5_CSA.vcf.gz"), chrompos=chrpos)
-csa <- csa %>% vcf_to_tibble() %>%
-    mutate(pop="AFR")
-csa
+# SAS
+sas <- fread(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/scratch", "FE_sas_exclu_whi_jhs_23andMe_qced_rsid.txt.gz"))
+sas <- subset(sas, rsid %in% prot$rsid)
+dim(sas)
+
 
 # EAS
-eas <- fread(here("scratch", "mdd_eas.txt.gz"))
-eas <- subset(eas, MarkerName %in% prot$rsid)
+eas <- fread(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/scratch", "FE_eas_exclu_whi_jhs_23andMe_qced_rsid.txt.gz"))
+eas <- subset(eas, rsid %in% prot$rsid)
 dim(eas)
 
+#HIS
+his <- fread(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/scratch", "FE_his_exclu_whi_jhs_23andMe_qced_rsid.txt.gz"))
+his <- subset(eas, rsid %in% prot$rsid)
+dim(his)
+
 # EUR
-eur <- fread(here("scratch", "mdd_eur.txt.gz"))
+eur <- fread(file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/scratch", "PGC_UKB_depression_genome-wide.txt"))
 eur <- subset(eur, MarkerName %in% prot$rsid)
 dim(eur)
 
-save(afr, csa, eas, eur, file=here("data", "mdd_extract.rdata"))
+save(afr, sas, eas, his, eur, file=file.path("C:/Users/HP OMEN GAMING/Desktop/mr-uganda-hackathon/data", "mdd_extract.rdata"))
 
